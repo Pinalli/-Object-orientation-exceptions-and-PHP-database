329bbbe0927436e51ab1219f380ec919f8cee22c
Pinalli <betopinalli@gmail.com>
2020-12-21 16:28:11 -0300

    Iniciado o curso

---
+++ Conta/ContaBancaria.php
@@ -0,0 +1,38 @@
+<?php
+
+class ContaBancaria
+{
+
+    private string $banco;
+    private string $nomeTitular;
+    private string $numAgencia;
+    private string $numConta;
+    private float $saldo;
+
+
+    public function __construct($banco, $nomeTitular, $numAgencia, $numConta, $saldo)
+   {
+       $this -> banco = $banco;
+       $this -> nomeTitular = $nomeTitular;
+       $this -> numAgencia = $numAgencia;
+       $this -> numConta = $numConta;
+       $this -> saldo = $saldo;
+   }
+
+   public function getSaldo()
+   {
+      return 'Seu saldo atual é de : ' . $this -> saldo;
+   }
+
+   public function depositar($valor)
+   {
+     $this -> saldo += $valor;
+     return 'Depósito de R$ ' .$valor .' realizado';
+   }
+
+   public function sacar($valor)
+   {
+       $this -> saldo -= $valor;
+       return 'Saque de R$ ' .$valor .' realizado';
+   }
+}
